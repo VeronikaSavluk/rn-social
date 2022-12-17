e6f6a104f648059f1ab0de61db454b009f509c91
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-12-17 02:23:14 +0200

    Create CreatePostsScreen.js

---
+++ Screens/mainScreen/CreatePostsScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  View,
+  Text,
+} from "react-native";
+
+import {styles} from '../../styles';
+
+const CreatePostsScreen = () =>{
+  return (
+    <View style={styles.container}>
+      <Text>CreatePostsScreen</Text>
+    </View>
+  );
+};
+
+export default CreatePostsScreen;
