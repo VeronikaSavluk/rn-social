3a42e9d786c15f6896e205aa65c954705f93175e
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-12-17 02:23:52 +0200

    Create ProfileScreen.js

---
+++ Screens/mainScreen/ProfileScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  View,
+  Text,
+} from "react-native";
+import {styles} from '../../styles';
+
+const ProfileScreen = () =>{
+  return (
+    <View style={styles.container}>
+      <Text>ProfileScreen</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
