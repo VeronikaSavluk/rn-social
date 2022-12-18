7c3d93b5479f6690fac21a26b69bd1edbee48e9b
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-12-18 14:44:44 +0200

    replaced&updated file CommentsScreen

---
+++ Screens/nestedScreen/CommentsScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  View,
+  Text,
+} from "react-native";
+import {styles} from '../../styles';
+
+const CommentsScreen = () =>{
+  return (
+    <View style={styles.container}>
+      <Text>CommentsScreen</Text>
+    </View>
+  );
+};
+
+export default CommentsScreen;
