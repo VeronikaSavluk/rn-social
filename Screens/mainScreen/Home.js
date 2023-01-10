d50eb13fef160ad3c46f9b68185a680c49551181
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-01-10 23:32:15 +0200

    added styles

--- Screens/mainScreen/Home.js
+++ Screens/mainScreen/Home.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { moduleName,
   TouchableOpacity,
   Image
  } from "react-native";
 import { useDispatch } from "react-redux";
 import { createStackNavigator } from "@react-navigation/stack";
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { authSignOutUser } from "../../redux/auth/authOperations";
 
 import PostsScreen from "../nestedScreen/PostsScreen";
@@ -14,8 +15,17 @@ import logOut from '../../images/log-out.png';
 
 const NestedScreen = createStackNavigator();
 
-const Home = () => {
+const Home = ({ navigation, route }) => {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const routeName = getFocusedRouteNameFromRoute(route);
+    if(routeName === 'Comments'){
+    navigation.setOptions({tabBarStyle: { display: 'none' }});
+    } else {
+    navigation.setOptions({tabBarStyle: { display: 'flex' }});
+    }
+  }, [route]);
 
   const signOut = () => {
     dispatch(authSignOutUser());
@@ -23,16 +33,26 @@ const Home = () => {
 
   return (
       <NestedScreen.Navigator>
-      <NestedScreen.Screen name='PostsScreen' component={PostsScreen}
-      options={{ headerTitleAlign: 'center',
-      headerRight: () => (
-      <TouchableOpacity onPressIn={signOut} style={{marginRight: 16}}>
-        <Image source={logOut}/>
-      </TouchableOpacity>
-      ),
-      }}/>
-      <NestedScreen.Screen name='Comments' component={CommentsScreen} options={{ headerTitleAlign: 'center' }}/>
-      <NestedScreen.Screen name='Map' component={MapScreen} options={{ headerTitleAlign: 'center' }}/>
+      <NestedScreen.Screen
+        name='PostsScreen'
+        component={PostsScreen}
+        options={{
+          headerTitleAlign: 'center',
+          headerRight: () => (
+            <TouchableOpacity onPressIn={signOut} style={{marginRight: 16}}>
+              <Image source={logOut}/>
+            </TouchableOpacity>
+          )
+        }}
+      />
+      <NestedScreen.Screen name='Comments'
+        component={CommentsScreen}
+        options={{ headerTitleAlign: 'center' }}
+        />
+      <NestedScreen.Screen
+        name='Map'
+        component={MapScreen}
+        options={{ headerTitleAlign: 'center' }}/>
     </NestedScreen.Navigator>
   );
 };
